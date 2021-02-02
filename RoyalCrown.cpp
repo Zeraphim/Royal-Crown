98b3c315422323e9773e35f8ae77a21ab0a8e9c8
Zeraphim <38757877+Zeraphim@users.noreply.github.com>
2021-02-02 10:33:33 +0800

    Add files via upload
    Program structure

---
+++ RoyalCrown.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+struct Students {
+	string givenName, lastName, courseCode;
+	int Q1, Q2, Q3, A1, A2, A3, A4, FE;
+	float grdTotal, grdEquivalent;
+};
+
+int main(){
+	
+	
+	//Menu
+	
+	
+	return 0;
+}
